1d96ea8275e1593fed281d8a684b219b591fa3cc
Dev Saraf <devsaraf297@gmail.com>
2026-07-11 22:20:39 +0530

    Google Agent New

---
+++ apps/2_google_agent.py
@@ -0,0 +1,30 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_community.utilities import GoogleSerperAPIWrapper
+from langchain_groq import ChatGroq
+from langchain.agents import create_agent
+
+model = ChatGroq(model = "openai/gpt-oss-20b")
+search = GoogleSerperAPIWrapper()
+
+agent = create_agent(
+    model = model,
+    tools = [
+        search.run
+    ],
+    system_prompt = "You are a agent and can search for any question on google."
+
+)
+
+while True:
+    query = input("User :").strip()
+    if not query:
+        continue
+
+    if query.lower() in ["exit", "quit", "bye"]:
+        print("Good bye! 👋")
+        break
+
+    response = agent.invoke({"messages":[{"role":"user", "content": query}]})
+    print("AI:", response['messages'][-1].content)
