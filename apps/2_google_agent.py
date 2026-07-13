49e4be99e55f5ea1cc249af69a2ee3e953a93d25
Dev Saraf <devsaraf297@gmail.com>
2026-07-13 18:38:57 +0530

    Google Search Agent with Memory

--- apps/2_google_agent.py
+++ apps/2_google_agent.py
@@ -4,16 +4,19 @@ load_dotenv()
 from langchain_community.utilities import GoogleSerperAPIWrapper
 from langchain_groq import ChatGroq
 from langchain.agents import create_agent
+from langgraph.checkpoint.memory import MemorySaver
 
 model = ChatGroq(model = "openai/gpt-oss-20b")
 search = GoogleSerperAPIWrapper()
+memory = MemorySaver()
 
 agent = create_agent(
     model = model,
     tools = [
         search.run
     ],
-    system_prompt = "You are a agent and can search for any question on google."
+    system_prompt = "You are a agent and can search for any question on google.",
+    checkpointer = memory   
 
 )
 
@@ -26,5 +29,8 @@ while True:
         print("Good bye! 👋")
         break
 
-    response = agent.invoke({"messages":[{"role":"user", "content": query}]})
+    response = agent.invoke(
+        {"messages":[{"role":"user", "content": query}]},
+        {"configurable" : {"thread_id" : "1"}}
+        )
     print("AI:", response['messages'][-1].content)
